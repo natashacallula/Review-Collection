c1192178d450db41357133e0ab4b19cbc00215ed
Natasha <natashacallula@gmail.com>
2025-05-27 17:36:10 +0700

    Progress

---
+++ Javascript/variable.js
@@ -0,0 +1,22 @@
+// Code 1
+// const nama = "Natasha";
+// console.log(nama);
+
+// Code 2
+// const mahasiswa = {
+//   nama : 'Natasha',
+//    kelas : 201,
+// }
+// mahasiswa.fakultas = 'Fisika'
+// console.log(mahasiswa);
+
+//Code 3
+const mahasiswa = ['Natasha', 'Callula'];
+mahasiswa.push('Liora');
+console.log(mahasiswa);
+
+//Code 4
+let x = "10" + 10 + 10;
+let y = 10 + 10 + "10";
+console.log("Hasil dari x : " + x);
+console.log("Hasil dari y : " + y);
